b059155f71db38d6d8092f3c70d56da5e2414614
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2022-03-13 22:44:47 -0300

    Delete Others directory

--- Others/2.py
+++
@@ -1,8 +0,0 @@
-number = int(input("\nType a number: "))
-
-if (number % 4 == 0):
-  print("\n%s is even and a multiple of 4." % number)
-elif (number % 2 == 0):
-  print("\n%s is even." % number)
-else:
-  print("\n%s is odd." % number)
